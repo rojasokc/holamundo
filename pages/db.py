c81b9a669435437023967f62035256dab15f6dbb
Victor Rojas <rojasokc@yahoo.com>
2026-03-18 18:00:44 -0500

    conexion mysql railway

--- pages/db.py
+++ pages/db.py
@@ -8,13 +8,25 @@
 #        database="tucodata"     # tu base de datos
 #    )
 
+#import mysql.connector
+#import os
+
+#def get_db_connection():
+#    return mysql.connector.connect(
+#        host=os.getenv("DB_HOST"),
+#        user=os.getenv("DB_USER"),
+#        password=os.getenv("DB_PASSWORD"),
+#        database=os.getenv("DB_NAME")
+#    )
+
 import mysql.connector
 import os
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT", 3306))
     )
