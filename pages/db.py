b056b3d1630eaa716b64fae2c3daac2fae9b7d29
Victor Rojas <rojasokc@yahoo.com>
2026-03-19 15:53:01 -0500

    ahora si

--- pages/db.py
+++ pages/db.py
@@ -2,20 +2,12 @@
 
 import os
 import mysql.connector
-from urllib.parse import urlparse
 
 def get_db_connection():
-    url = os.getenv("DATABASE_URL")
-
-    if not url:
-        raise Exception("DATABASE_URL no está definida")
-
-    parsed = urlparse(url)
-
     return mysql.connector.connect(
-        host=parsed.hostname,
-        port=parsed.port,
-        user=parsed.username,
-        password=parsed.password,
-        database=parsed.path.lstrip('/')
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),  # 👈 ESTO FALTABA
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
