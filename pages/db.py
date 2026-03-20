81c473db08ef00fede398bb654c29e1606ba8cfb
Victor Rojas <rojasokc@yahoo.com>
2026-03-19 21:24:50 -0500

    otra vez

--- pages/db.py
+++ pages/db.py
@@ -1,15 +1,13 @@
-
-
 import os
 import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        port=int(os.getenv("DB_PORT")),  # 👈 ESTO FALTABA
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        host=os.getenv("MYSQLHOST"),
+        port=int(os.getenv("MYSQLPORT")),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE")
     )
 
 print("DB_HOST:", os.getenv("DB_HOST"))
